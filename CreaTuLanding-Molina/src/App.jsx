c427ccf71897b4e014d0304a960381337ab50969
Rocío Molina <rociomolina.b7@gmail.com>
2023-10-10 13:16:29 -0300

    "CreaTuLanding+Molina"

--- CreaTuLanding-Molina/src/App.jsx
+++ CreaTuLanding-Molina/src/App.jsx
@@ -4,8 +4,6 @@ import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
- 
-
   return (
     <div className='app'>
         <NavBar/>
--- CreaTuLanding-Molina/src/components/NavBar/NavBar.jsx
+++ CreaTuLanding-Molina/src/components/NavBar/NavBar.jsx
@@ -3,14 +3,20 @@ import CartWidget from "../CartWidget/CartWidget";
 const NavBar = () =>
 {
  return (
-    <nav>
-        <h3>E-commerce.</h3>
-        <div>
-            <button>Veganos</button>
-            <button>Vegetarianos</button>
-            <button>Sin Tacc</button>
+    <nav className="navbar bg-dark text-light mb-5">
+        <div className="container-fluid">
+        <h3 className="col">E-commerce Healthy Market.</h3>
+        <div className="col d-flex justify-content-evenly" >
+            <button className="btn btn-primary">Veganos</button>
+            <button className="btn btn-secondary">Vegetarianos</button>
+            <button className="btn btn-primary">Sin Tacc</button>
         </div>
+        <div className="col">
         <CartWidget/>
+        </div>
+       
+        </div>
+        
     </nav>
 
 )
