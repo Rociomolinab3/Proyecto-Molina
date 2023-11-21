b478f783b1b4b5130dcb1bc8804504513175c333
Rocío Molina <rociomolina.b3@gmail.com>
2023-11-21 18:39:13 -0300

    "spinner"

---
+++ CreaTuLanding-Molina/src/components/Loader/Spinner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from '../Spinner';
+
+const Spinner = () =>{
+
+    return(
+        <div className="lds-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    )
+}
+
+export default Loader;
